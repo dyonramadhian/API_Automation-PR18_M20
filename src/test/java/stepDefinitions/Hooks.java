e7c7cb0524ddcdfa6e979760a6a2bfb2ad3621ce
dyonramadhian <ramadhianrg@gmail.com>
2025-04-14 14:40:20 +0700

    v2

--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -1,23 +1,28 @@
 package stepDefinitions;
 
-//public class Hooks {
-//    @BeforeAll
-//    public static void setUp() {
-//        System.out.println("Before All");
-//    }
-//
-//    @AfterAll
-//    public static void tearDownAll() {
-//        System.out.println("After All");
-//    }
-//
-//    @Before
-//    public static void beforeTest() {
-//        System.out.println("Before Test");
-//    }
-//
-//    @Before
-//    public static void afterTest() {
-//        System.out.println("After Test");
-//    }
-//}
+import org.junit.After;
+import org.junit.Before;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+
+public class Hooks {
+    @BeforeAll
+    public static void setUp() {
+        System.out.println("Before All");
+    }
+
+    @AfterAll
+    public static void tearDownAll() {
+        System.out.println("After All");
+    }
+
+    @Before
+    public void beforeTest() {
+        System.out.println("Before Test");
+    }
+
+    @After
+    public void afterTest() {
+        System.out.println("After Test");
+    }
+}
